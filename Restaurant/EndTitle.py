5d7160cedb0e9bfaea80a46cbb9975de81d94fdf
ClassicalGalileo <70212347+ClassicalGalileo@users.noreply.github.com>
2020-10-05 20:24:55 -0400

    Update EndTitle.py

--- Restaurant/EndTitle.py
+++ Restaurant/EndTitle.py
@@ -1,8 +1,1 @@
-from Porject import *
-
-def endTitle():
-	print("Great! " + str(plate) + ", and " + str(liquid) + " it is!")
-	input("Press Enter to Continue...")
-	print("Ok sir/Ma'am, here's the bill: ")
-	caluculation = costPlate + costLiquid
-	print(caluculation + " dollars, thank you for coming")
+ignore this file
